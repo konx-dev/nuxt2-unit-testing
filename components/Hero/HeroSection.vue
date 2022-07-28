03eb3b9c802170cc27286cfc3efc8135423bd7f4
Oliver Cox <olivercoxdesign@gmail.com>
2022-07-28 09:21:42 +0100

    added some intial tests

--- components/Hero/HeroSection.vue
+++ components/Hero/HeroSection.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- This example requires Tailwind CSS v2.0+ -->
-    <div class="relative bg-white overflow-hidden">
+    <div v-if="ctx" class="relative bg-white overflow-hidden">
         <div class="max-w-7xl mx-auto">
             <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
                 <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
@@ -16,52 +16,16 @@
                                         <span class="sr-only">Workflow</span>
                                         <img alt="Workflow" class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg">
                                     </a>
-                                    <div class="-mr-2 flex items-center md:hidden">
-                                        <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false" @click="menuState = true">
-                                            <span class="sr-only">Open main menu</span>
-                                            <div class="h-6 w-6">
-                                                <Burger />
-                                            </div>
-                                        </button>
-                                    </div>
                                 </div>
-                            </div>
-                            <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-                                <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Product</a>
-                                <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Features</a>
-                                <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Marketplace</a>
-                                <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Company</a>
                             </div>
                         </nav>
-                    </div>
-                    <div v-if="menuState" class="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-                        <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                            <div class="px-5 pt-4 flex items-center justify-between">
-                                <div>
-                                    <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="">
-                                </div>
-                                <div class="-mr-2">
-                                    <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" @click="menuState = false">
-                                        <span class="sr-only">Close main menu</span>
-                                        <div class="h-6 w-6">
-                                            <Cross />
-                                        </div>
-                                    </button>
-                                </div>
-                            </div>
-                            <div class="px-2 pt-2 pb-3 space-y-1">
-                                <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Product</a>
-                                <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Features</a>
-                                <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Marketplace</a>
-                                <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Company</a>
-                            </div>
-                        </div>
                     </div>
                 </div>
 
                 <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                     <div class="sm:text-center lg:text-left">
-                        <h1 class="hero__heading text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl" v-html="ctx.heading" />
+                        <!-- <h1 data-testid="hero-header" class="hero__heading text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl" v-html="ctx.heading" /> -->
+                        <h1 data-testid="hero-header" class="hero__heading text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">{{ ctx.heading }}</h1>
                         <p v-if="ctx.body" class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                             {{ ctx.body }}
                         </p>
@@ -78,28 +42,16 @@
             </div>
         </div>
         <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-            <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" :src="ctx.image.url" :alt="ctx.image.url">
+            <img data-testid="hero-image" class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" :src="ctx.image.url" :alt="ctx.image.url">
         </div>
     </div>
 </template>
 <script>
-import Cross from '@/assets/svg/cross.svg'
-import Burger from '@/assets/svg/burger.svg'
-
 export default {
-  components: {
-    Cross,
-    Burger
-  },
+  components: {},
   props: {
     ctx: {
-      type: Object,
-      required: true
-    }
-  },
-  data () {
-    return {
-      menuState: false
+      type: Object
     }
   }
 }
---
+++ test/HeroSection.spec.js
@@ -0,0 +1,103 @@
+/*
+
+-- Testing for a generic Hero component -- 
+
+Assumptions:
+The data layer has been setup appropriately with field limits and required flags
+The component will only ever be used in relation to the corresponding cms field
+
+Fields:
+Heading * - Redactor 
+Body - Plaintext
+Image * (limit of 1) - Asset
+Buttons (0, 1 or 2) - Matrix field
+
+Example data object:
+
+heroContent: {
+    heading: '<p>Data to enrich your <em>online business</em></p>',
+    body: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.',
+    image: { title: 'Placeholder image', url: 'https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80' },
+    buttons: [
+        { text: 'Get started', link: '/' },
+        { text: 'Live demo', link: '/' }
+    ]
+}
+
+Tests: 
+
+[1.] Component renders and all required fields are output to the template - Requirement tbc
+[2.] The heading is rendered and is utilising a H1, dom element: data-testid="hero-header"
+[3.] The image has rendered and has an alt tag, dom element: data-testid="hero-image"
+[4.] Available optional fields are rendered as expected, dom element: data-testid="hero-body" | data-testid="hero-buttons" 
+[5.] Unavailable optional fields aren't trying to be rendered
+[6.] Clicking the buttons has a response, dom element: data-testid="hero-button"
+
+*/
+
+import { mount } from "@vue/test-utils";
+import HeroSection from '@/components/Hero/HeroSection.vue';
+
+const sampleData = {
+    heading: 'Hero Demo Title',
+    body: 'Hero Demo body copy here',
+    image: [
+        { title: 'An Image', url: '/an-image-filepath.jpg' }
+    ],
+    buttons: [
+        { title: 'button a', link: 'a-link.com/subdfolder' },
+        { title: 'button b', link: 'another-link.com/subfolder'}
+    ]
+}
+
+
+describe('HeroSection', () => {
+
+    test('[1.] Component renders and all required fields are output to the template', async () => {
+        
+        const wrapper = mount(HeroSection, {
+            propsData: {
+                ctx: {
+                    heading: '<p>Data to enrich your <em>online business</em></p>',
+                    body: null,
+                    image: { title: 'Placeholder image', url: 'https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80' },
+                    buttons: []
+                }
+            }
+        })
+
+        const heading = wrapper.find('[data-testid=hero-header]')
+        const image = wrapper.find('[data-testid=hero-image]')
+
+        expect(heading.exists()).toBeTruthy()
+        expect(image.exists()).toBeTruthy()
+    })
+    // TEST INCOMPLETE
+    test('[2.] The heading is rendering the correct field and is utilising a H1, dom element: data-testid="hero-header"', () => {
+        
+        const wrapper = mount(HeroSection, {
+            propsData: {
+                ctx: sampleData
+            }
+        })
+        const h1 = wrapper.find('h1')
+        const headingText = h1.text()
+        
+        expect(wrapper.find('h1').exists()).toBe(true);
+        expect(h1.text()).toContain(sampleData.heading)
+    })
+
+    test('[3.] The image has rendered and has an alt tag, dom element: data-testid="hero-image"', () => {
+        
+        const wrapper = mount(HeroSection, {
+            propsData: {
+                ctx: sampleData
+            }
+        })
+
+        const image = wrapper.find('[data-testid=hero-image]')
+
+        expect(image.exists()).toBeTruthy()
+    })
+
+})
